c82057aad17ed372310d298886bca34387290750
dracu-lah <dracu@PC>
2024-10-20 10:35:50 +0530

    feat: added get employees feature

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employees =dbContext.Employees.ToList();
+            return Ok(employees);
+        }
+    }
+}
