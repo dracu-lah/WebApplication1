01407340e7a55d05047e61908997e09cd0955266
unknown <dracu@PC>
2024-10-20 10:20:28 +0530

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
